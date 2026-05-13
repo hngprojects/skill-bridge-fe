c32afbb225374b808f0341a11096fa57b92cb10c
Ayomikun <ayoadeosun10@gmail.com>
2026-05-13 13:15:44 +0100

    chore: migrate utils hooks and folder structure

--- src/app/(external)/terms-of-use/page.tsx
+++ src/app/(external)/terms-of-use/page.tsx
@@ -1,4 +1,4 @@
-import TermsOfUsePage from "@/components/termsofuse/terms-of-use-page";
+import TermsOfUsePage from "@/components/terms-of-use/terms-of-use-page";
 
 const Page = () => {
   return <TermsOfUsePage />;
